7201414e23aa114a367ffd7c2fde0868205ab122
github-action-benchmark <github@users.noreply.github.com>
2026-02-03 01:31:00 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 18c41ba5c77eb8f4277237b7d4fbf2612a0739dd

--- benchmark-data/data.js
+++ benchmark-data/data.js
@@ -1,1 +1,72 @@
-{}
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1770082260214,
+  "repoUrl": "https://github.com/albert-einshutoin/lazy-image",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "75466198+albert-einshutoin@users.noreply.github.com",
+            "name": "Albert Einshutoin",
+            "username": "albert-einshutoin"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "18c41ba5c77eb8f4277237b7d4fbf2612a0739dd",
+          "message": "Merge pull request #328 from albert-einshutoin/release/v0.10.0\n\nfix(ci): prepare working tree before gh-pages switch on main push",
+          "timestamp": "2026-02-03T10:26:15+09:00",
+          "tree_id": "3d472ed5f1a8f681a8a7f4e2b4485350b9106fc4",
+          "url": "https://github.com/albert-einshutoin/lazy-image/commit/18c41ba5c77eb8f4277237b7d4fbf2612a0739dd"
+        },
+        "date": 1770082259912,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "JPEG size (lazy-image)",
+            "value": 31518,
+            "unit": "bytes"
+          },
+          {
+            "name": "JPEG time (lazy-image)",
+            "value": 129,
+            "unit": "ms"
+          },
+          {
+            "name": "WebP size (lazy-image)",
+            "value": 32448,
+            "unit": "bytes"
+          },
+          {
+            "name": "WebP time (lazy-image)",
+            "value": 420,
+            "unit": "ms"
+          },
+          {
+            "name": "AVIF size (lazy-image)",
+            "value": 28666,
+            "unit": "bytes"
+          },
+          {
+            "name": "AVIF time (lazy-image)",
+            "value": 800,
+            "unit": "ms"
+          },
+          {
+            "name": "Complex pipeline size (lazy-image)",
+            "value": 27129,
+            "unit": "bytes"
+          },
+          {
+            "name": "Complex pipeline time (lazy-image)",
+            "value": 125,
+            "unit": "ms"
+          }
+        ]
+      }
+    ]
+  }
+}
